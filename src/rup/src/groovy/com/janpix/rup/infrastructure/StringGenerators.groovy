3df9de771356fb7405d45046707473ce097b75e6
Joaquin Magneres <joacomag@gmail.com>
2013-04-01 17:11:41 -0300

    Arreglo el mock de uuid para que utilice randomUUID

--- src/rup/src/groovy/com/janpix/rup/infrastructure/StringGenerators.groovy
+++ src/rup/src/groovy/com/janpix/rup/infrastructure/StringGenerators.groovy
@@ -1,18 +1,7 @@
 package com.janpix.rup.infrastructure
-import java.util.UUID
-
-import com.janpix.rup.empi.PatientIdentifier;
 
 class MockUUIDGenerator {
 	static def getUUID() {
-		//def uuid = UUID.randomUUID()
-		/*def uuid = "1"
-		if(PatientIdentifier.count > 0)
-			//uuid = PatientIdentifier.find {}
-			uuid = "2"*/
-			
-		//return {uuid.toString()}
-		//return uuid
-		return {"5"}
+		return { UUID.randomUUID().toString() }
 	}
 }
