1dbb627f4d9231351ebcc1d3b1982fc3afe8bf7f
Joaquin Magneres <joacomag@gmail.com>
2013-04-13 18:53:35 -0300

    Mejoro el mapeo del mensaje al objeto del dominio. Hago un test,

--- src/rup/src/groovy/com/janpix/rup/services/mappings/PIXContractMapper.groovy
+++ src/rup/src/groovy/com/janpix/rup/services/mappings/PIXContractMapper.groovy
@@ -1,8 +1,11 @@
 package com.janpix.rup.services.mappings
 
+import javax.xml.bind.JAXBElement;
+
 import org.hl7.v3.*
 
 import com.janpix.rup.empi.Person
+import com.janpix.rup.empi.PersonName
 import com.janpix.rup.exceptions.MessageMappingException
 
 /**
@@ -19,10 +22,15 @@ class PIXContractMapper {
 		validateHl7V3AddNewPatientMessage(inPatientMessage)
 		def person = new Person()
 		PRPAIN201301UV02MFMIMT700701UV01RegistrationEvent regEvent = inPatientMessage.controlActProcess.subject[0].registrationEvent
-		PRPAMT201301UV02Person patientPerson = regEvent.subject1.patient.patientPerson.getValue() 
-		/*person.givenName = patientPerson.name.each { 
-			PN name = it as PN
-		} */
+		PRPAMT201301UV02Person patientPerson = regEvent.subject1.patient.patientPerson.getValue()
+		patientPerson.name.each { PN it ->
+			def givenNameJaxb = it.content.find() { 
+				def jaxbElement = it as JAXBElement<Serializable>
+				return jaxbElement.name.localPart == "given" 
+			} as JAXBElement<Serializable>
+			person.givenName = new PersonName(firstName: "${givenNameJaxb.value}") 
+		}
+		return person
 	}
 	
 	private static void validateHl7V3AddNewPatientMessage(PRPAIN201301UV02 inPatientMessage) {
---
+++ src/rup/test/unit/com/janpix/rup/services/mappings/PIXContractMapperTest.groovy
@@ -0,0 +1,41 @@
+package com.janpix.rup.services.mappings;
+
+import grails.test.mixin.*
+import javax.xml.bind.JAXBElement
+import javax.xml.namespace.QName
+
+import org.hl7.v3.EnGiven
+import org.hl7.v3.PN
+import org.hl7.v3.PRPAIN201301UV02
+import org.hl7.v3.PRPAIN201301UV02MFMIMT700701UV01ControlActProcess;
+import org.hl7.v3.PRPAIN201301UV02MFMIMT700701UV01RegistrationEvent;
+import org.hl7.v3.PRPAIN201301UV02MFMIMT700701UV01Subject1;
+import org.hl7.v3.PRPAIN201301UV02MFMIMT700701UV01Subject2;
+import org.hl7.v3.PRPAMT201301UV02Patient
+import org.hl7.v3.PRPAMT201301UV02Person
+
+class PIXContractMapperTest {
+
+	@Test
+	public void whenHl7AddPatientObjectHasNameThenCheckItReturnsAPatientDomainObjectWithTheSameName() {
+		
+		//BUILD ADD PATIENT MESSAGE WITH JUAN NAME
+		def patientName = new PN()
+		patientName.content.add(new JAXBElement<EnGiven>(new QName("given"), JAXBElement.class, "Juan"))
+		def patientPerson = new PRPAMT201301UV02Person()
+		patientPerson.name.add(patientName)
+		def subjectControlActProcess = new PRPAIN201301UV02MFMIMT700701UV01Subject1()
+		subjectControlActProcess.registrationEvent = new PRPAIN201301UV02MFMIMT700701UV01RegistrationEvent()
+		subjectControlActProcess.registrationEvent.subject1 = new PRPAIN201301UV02MFMIMT700701UV01Subject2()
+		subjectControlActProcess.registrationEvent.subject1.patient = new PRPAMT201301UV02Patient()
+		subjectControlActProcess.registrationEvent.subject1.patient.patientPerson = new JAXBElement<PRPAMT201301UV02Person>(new QName("patientPerson"), JAXBElement.class, patientPerson)
+		def inPatientMessage = new PRPAIN201301UV02()
+		inPatientMessage.controlActProcess = new PRPAIN201301UV02MFMIMT700701UV01ControlActProcess()
+		inPatientMessage.controlActProcess.subject.add(subjectControlActProcess)
+		
+		def patient = PIXContractMapper.mapPersonFromhl7v3AddNewPatientMessage(inPatientMessage)
+
+		assert patient.givenName.firstName == "Juan"
+	}
+
+}
