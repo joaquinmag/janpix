124fe5ccee5b780bf35d9cfdcfdc63bde31245a0
Joaquin Magneres <joacomag@gmail.com>
2013-04-17 01:28:43 -0300

    Add birthDate and respective test

--- src/rup/src/groovy/com/janpix/rup/services/mappings/PIXContractMapper.groovy
+++ src/rup/src/groovy/com/janpix/rup/services/mappings/PIXContractMapper.groovy
@@ -4,6 +4,7 @@ import javax.xml.bind.JAXBElement;
 
 import org.hl7.v3.*
 
+import com.janpix.rup.empi.ExtendedDate
 import com.janpix.rup.empi.Person
 import com.janpix.rup.empi.PersonName
 import com.janpix.rup.exceptions.MessageMappingException
@@ -20,13 +21,45 @@ class PIXContractMapper {
 	 */
 	static Person mapPersonFromhl7v3AddNewPatientMessage(PRPAIN201301UV02 inPatientMessage) {
 		validateHl7V3AddNewPatientMessage(inPatientMessage)
-		def person = new Person()
+		
 		PRPAIN201301UV02MFMIMT700701UV01RegistrationEvent regEvent = inPatientMessage.controlActProcess.subject[0].registrationEvent
 		PRPAMT201301UV02Person patientPerson = regEvent.subject1.patient.patientPerson.getValue()
+		
+		//set name
+		def person = new Person()
 		person.givenName = new PersonName()
 		person.givenName.firstName = getNameFromPatient(patientPerson, "given")?.toString()
 		person.givenName.lastName = getNameFromPatient(patientPerson, "family")?.toString()
+		person.administrativeSex =  patientPerson.administrativeGenderCode.code
+		person.birthdate = convertToExtendedDateFromTS(patientPerson.birthTime)
+		
 		return person
+	}
+	
+	private static ExtendedDate convertToExtendedDateFromTS(TS hl7Date) {
+		def precission = ExtendedDate.TYPE_PRECISSION_UNKNOWN
+		def year = 1900
+		def month = 1
+		def day = 1
+		if (hl7Date?.value?.length() >= 4) {
+			year = hl7Date.value.substring(0, 4) as int
+			precission = ExtendedDate.TYPE_PRECISSION_YEAR
+		}
+		if (hl7Date?.value?.length() >= 6) {
+			month = hl7Date.value.substring(4, 6) as int
+			precission = ExtendedDate.TYPE_PRECISSION_MONTH
+		}
+		if (hl7Date?.value?.length() >= 8) {
+			day = hl7Date.value.substring(6, 8) as int
+			precission = ExtendedDate.TYPE_PRECISSION_DAY
+		}
+		def date = buildDate(year, month, day)
+		return new ExtendedDate(date: date, precission: precission)
+	}
+	
+	private static Date buildDate(int year, int month, int day) {
+		def calendar = new GregorianCalendar(year, month, day)
+		return calendar.getTime()
 	}
 	
 	/**
@@ -55,6 +88,8 @@ class PIXContractMapper {
 			throw new MessageMappingException("PRPAIN201301UV02 message must contain one subject")
 		if (inPatientMessage.getControlActProcess().getSubject().size() > 1)
 			throw new MessageMappingException("PRPAIN201301UV02 message can't contain more than one subject at this implementation")
+		if (inPatientMessage.controlActProcess.subject[0].registrationEvent.subject1.patient.patientPerson.value.administrativeGenderCode == null)
+			throw new MessageMappingException("PRPAIN201301UV02 message must contain an administrativeGenderCode")
 	}
 	
 }
--- src/rup/test/unit/com/janpix/rup/services/mappings/PIXContractMapperTest.groovy
+++ src/rup/test/unit/com/janpix/rup/services/mappings/PIXContractMapperTest.groovy
@@ -5,6 +5,7 @@ import grails.test.mixin.*
 import javax.xml.bind.JAXBElement
 import javax.xml.namespace.QName
 
+import org.hl7.v3.CE
 import org.hl7.v3.EnGiven
 import org.hl7.v3.PN
 import org.hl7.v3.PRPAIN201301UV02
@@ -14,6 +15,7 @@ import org.hl7.v3.PRPAIN201301UV02MFMIMT700701UV01Subject1
 import org.hl7.v3.PRPAIN201301UV02MFMIMT700701UV01Subject2
 import org.hl7.v3.PRPAMT201301UV02Patient
 import org.hl7.v3.PRPAMT201301UV02Person
+import org.hl7.v3.TS
 
 class PIXContractMapperTest {
 
@@ -26,6 +28,7 @@ class PIXContractMapperTest {
 		patientName.content.add(new JAXBElement<EnGiven>(new QName("family"), JAXBElement.class, "Perez García"))
 		def patientPerson = new PRPAMT201301UV02Person()
 		patientPerson.name.add(patientName)
+		patientPerson.administrativeGenderCode = new CE(code: "M") 
 		def subjectControlActProcess = new PRPAIN201301UV02MFMIMT700701UV01Subject1()
 		subjectControlActProcess.registrationEvent = new PRPAIN201301UV02MFMIMT700701UV01RegistrationEvent()
 		subjectControlActProcess.registrationEvent.subject1 = new PRPAIN201301UV02MFMIMT700701UV01Subject2()
@@ -49,6 +52,7 @@ class PIXContractMapperTest {
 		patientName.content.add(new JAXBElement<EnGiven>(new QName("family"), JAXBElement.class, "Perez García"))
 		def patientPerson = new PRPAMT201301UV02Person()
 		patientPerson.name.add(patientName)
+		patientPerson.administrativeGenderCode = new CE(code: "M")
 		def subjectControlActProcess = new PRPAIN201301UV02MFMIMT700701UV01Subject1()
 		subjectControlActProcess.registrationEvent = new PRPAIN201301UV02MFMIMT700701UV01RegistrationEvent()
 		subjectControlActProcess.registrationEvent.subject1 = new PRPAIN201301UV02MFMIMT700701UV01Subject2()
@@ -62,6 +66,29 @@ class PIXContractMapperTest {
 
 		assert patient.givenName.firstName == null
 		assert patient.givenName.lastName == "Perez García"
+	}
+	
+	@Test
+	public void whenHl7AddPatientObjectHasBirthTimeCompleteCheckThePersonHasDayPrecissionAndCorrectDate() {
+		
+		def patientName = new PN()
+		patientName.content.add(new JAXBElement<EnGiven>(new QName("family"), JAXBElement.class, "Perez García"))
+		def patientPerson = new PRPAMT201301UV02Person()
+		patientPerson.name.add(patientName)
+		patientPerson.administrativeGenderCode = new CE(code: "M")
+		patientPerson.birthTime = new TS(value: "19850523")
+		def subjectControlActProcess = new PRPAIN201301UV02MFMIMT700701UV01Subject1()
+		subjectControlActProcess.registrationEvent = new PRPAIN201301UV02MFMIMT700701UV01RegistrationEvent()
+		subjectControlActProcess.registrationEvent.subject1 = new PRPAIN201301UV02MFMIMT700701UV01Subject2()
+		subjectControlActProcess.registrationEvent.subject1.patient = new PRPAMT201301UV02Patient()
+		subjectControlActProcess.registrationEvent.subject1.patient.patientPerson = new JAXBElement<PRPAMT201301UV02Person>(new QName("patientPerson"), JAXBElement.class, patientPerson)
+		def inPatientMessage = new PRPAIN201301UV02()
+		inPatientMessage.controlActProcess = new PRPAIN201301UV02MFMIMT700701UV01ControlActProcess()
+		inPatientMessage.controlActProcess.subject.add(subjectControlActProcess)
+		
+		def patient = PIXContractMapper.mapPersonFromhl7v3AddNewPatientMessage(inPatientMessage)
+
+		assert patient.birthdate.date == new GregorianCalendar(1985,5,23).getTime()
 	}
 
 }
