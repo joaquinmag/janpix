b095692e3f599943c7b13797275069f50ea9f14a
Joaquin Magneres <joacomag@gmail.com>
2013-04-26 13:29:23 -0300

    Agrego helper

--- src/rup/src/groovy/com/janpix/rup/services/helpers/Hl7v3MessageHelper.groovy
+++ src/rup/src/groovy/com/janpix/rup/services/helpers/Hl7v3MessageHelper.groovy
@@ -13,4 +13,8 @@ class Hl7v3MessageHelper {
 	TS buildHl7DateTime(Date date) {
 		return new TS(value: "${date.gcal.get(Calendar.YEAR)}${date.gcal.get(Calendar.MONTH)}${date.gcal.get(Calendar.DAY_OF_MONTH)}${date.gcal.get(Calendar.HOUR_OF_DAY)}${date.gcal.get(Calendar.MINUTE)}${date.gcal.get(Calendar.SECOND)}")
 	}
+	
+	II buildInteractionId(String messageRootName) {
+		return new II(root: "2.16.840.1.113883.1.6", extension: messageRootName)
+	}
 }
