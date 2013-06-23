523e1ddaa02c20b244f19cec29e6c418df1adae8
Martín Barnech <mgbarnech@gmail.com>
2013-06-23 20:26:22 -0300

    actualizo el submodulo utilizado

--- src/rup/grails-app/services/com/janpix/rup/pixmanager/PIXManagerHL7v3Service.groovy
+++ src/rup/grails-app/services/com/janpix/rup/pixmanager/PIXManagerHL7v3Service.groovy
@@ -5,6 +5,7 @@ import org.grails.cxf.utils.GrailsCxfEndpoint
 
 import com.janpix.hl7dto.hl7.v3.contracts.*;
 import com.janpix.hl7dto.hl7.v3.interfaces.PixManagerInterface;
+import com.janpix.hl7dto.hl7.v3.messages.HL7OperationMessage;
 
 @GrailsCxfEndpoint(expose = EndpointType.JAX_WS,soap12=true)
 class PIXManagerHL7v3Service implements PixManagerInterface  {
@@ -14,6 +15,7 @@ class PIXManagerHL7v3Service implements PixManagerInterface  {
 	def pixContractMapper
 		
 	
+	@Override
 	/**
 	 * Add new patients to the PIX. 
 	 */								
@@ -29,50 +31,32 @@ class PIXManagerHL7v3Service implements PixManagerInterface  {
 		return pixContractMapper.mapACKMessageToHL7AcceptAcknowledgmentMessage(ack, identifier,  receiver,  sender)
 	}
 
-//		
-//	/**
-//	 * Merges two patients that where added as different patients
-//	 * Patient Registry Duplicates Resolved (IHE_ITI Vol 2b - Seccion: 3.44.4)
-//	 * @param body PRPA_IN201304UV02 (ej: ftp://ftp.ihe.net/TF_Implementation_Material/ITI/examples/PIXV3/05_PatientRegistryDuplicatesResolved.xml )
-//	 * @return ACK : MCCI_IN000002UV01 @see <a href="ftp://ftp.ihe.net/TF_Implementation_Material/ITI/examples/PIXV3/02_PatientRegistryRecordAdded1Ack.xml>link</a>
-//	 */
-//	@WebResult(name = "MCCI_IN000002UV01", targetNamespace = "urn:hl7-org:v3", partName = "Body")
-//	@Action(input = "urn:hl7-org:v3:PRPA_IN201304UV02", output = "urn:hl7-org:v3:MCCI_IN000002UV01")
-//	@WebMethod(operationName = "PIXManager_PRPA_IN201304UV02", action = "urn:hl7-org:v3:PRPA_IN201304UV02")
-//	@WSDLDocumentation("Merges two patients that where added as different patients. Patient Registry Duplicates Resolved (IHE_ITI Vol 2b - Seccion: 3.44.4)")
-//	public org.hl7.v3.MCCIIN000002UV01 pixManagerPRPAIN201304UV02(
-//		@WebParam(partName = "Body", name = "PRPA_IN201304UV02", targetNamespace = "urn:hl7-org:v3")
-//		org.hl7.v3.PRPAIN201304UV02 body) {
-//	}
-//
-//	/**
-//	 * This method is for updating patient information
-//	 * Patient Registry Record Revised(IHE_ITI Vol 2b - Seccion: 3.44.4.1)
-//	 * @param body PRPA_IN201302UV02 @see <a href="ftp://ftp.ihe.net/TF_Implementation_Material/ITI/examples/PIXV3/04_PatientRegistryRecordRevised2.xml">link</a>
-//	 * @return ACK : MCCI_IN000002UV01 @see <a href="ftp://ftp.ihe.net/TF_Implementation_Material/ITI/examples/PIXV3/02_PatientRegistryRecordAdded1Ack.xml">link</a>
-//	 */
-//	@WebResult(name = "MCCI_IN000002UV01", targetNamespace = "urn:hl7-org:v3", partName = "Body")
-//	@Action(input = "urn:hl7-org:v3:PRPA_IN201302UV02", output = "urn:hl7-org:v3:MCCI_IN000002UV01")
-//	@WebMethod(operationName = "PIXManager_PRPA_IN201302UV02", action = "urn:hl7-org:v3:PRPA_IN201302UV02")
-//	@WSDLDocumentation("This method is for updating patient information. Patient Registry Record Revised(IHE_ITI Vol 2b - Seccion: 3.44.4.1)")
-//	public org.hl7.v3.MCCIIN000002UV01 pixManagerPRPAIN201302UV02 (
-//		@WebParam(partName = "Body", name = "PRPA_IN201302UV02", targetNamespace = "urn:hl7-org:v3")
-//		org.hl7.v3.PRPAIN201302UV02 body) {
-//	}
-//
-//	/**
-//	 * Returns all the identifiers of a patient.
-//	 * Patient Registry Get Identifiers Query (IHE_ITI Vol 2b - Seccion: 3.45.4)
-//	 * @param body PRPA_IN201309UV02 @see <a href="ftp://ftp.ihe.net/TF_Implementation_Material/ITI/examples/PIXV3/06_PIXQuery1.xml">link</a>
-//	 * @return PRPA_IN201310UV02 @see <a href="ftp://ftp.ihe.net/TF_Implementation_Material/ITI/examples/PIXV3/07_PIXQuery1Response.xml">link</a>
-//	 */
-//	@WebResult(name = "PRPA_IN201310UV02", targetNamespace = "urn:hl7-org:v3", partName = "Body")
-//	@Action(input = "urn:hl7-org:v3:PRPA_IN201309UV02", output = "urn:hl7-org:v3:PRPA_IN201310UV02")
-//	@WebMethod(operationName = "PIXManager_PRPA_IN201309UV02", action = "urn:hl7-org:v3:PRPA_IN201309UV02")
-//	@WSDLDocumentation("Returns all the identifiers of a patient. Patient Registry Get Identifiers Query (IHE_ITI Vol 2b - Seccion: 3.45.4)")
-//	public org.hl7.v3.PRPAIN201310UV02 pixManagerPRPAIN201309UV02(
-//		@WebParam(partName = "Body", name = "PRPA_IN201309UV02", targetNamespace = "urn:hl7-org:v3")
-//		org.hl7.v3.PRPAIN201309UV02 body) {
-//		
-//	}
+	@Override
+	/**
+	 * Merges two patients that where added as different patients
+	 */
+	public MCCIIN000002UV01 MergePatients(HL7OperationMessage body) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	/**
+	 * This method is for updating patient information
+	 */
+	public MCCIIN000002UV01 UpdatePatient(HL7OperationMessage body) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	/**
+	 * Returns all the identifiers of a patient.
+	 */
+	public HL7OperationMessage GetAllIdentifiersPatient(HL7OperationMessage body) {
+		// TODO Auto-generated method stub
+		return new HL7OperationMessage();
+	}
+
+
 }
