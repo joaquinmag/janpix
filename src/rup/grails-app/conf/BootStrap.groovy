b7a709fdad21468f416b0c3c18d89879c14513de
Martín Barnech <mgbarnech@gmail.com>
2013-07-05 18:40:30 -0300

    Agrego HealthEntity test en bootstrap

--- src/rup/grails-app/conf/BootStrap.groovy
+++ src/rup/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,8 @@
-import com.janpix.rup.empi.AuthorityRUP;
-import com.janpix.rup.empi.City;
-import com.janpix.rup.empi.Country;
-import com.janpix.rup.empi.Province;
+import com.janpix.rup.empi.AuthorityRUP
+import com.janpix.rup.empi.City
+import com.janpix.rup.empi.Country
+import com.janpix.rup.empi.HealthEntity
+import com.janpix.rup.empi.Province
 
 class BootStrap {
 
@@ -11,6 +12,10 @@ class BootStrap {
 		if (AuthorityRUP.count() == 0) {
 			new AuthorityRUP(grailsApplication.config.rup.authority.rupoid, grailsApplication.config.rup.authority.name ).save(flush: true, failOnError: true)
 		}
+		def testAuthorityOID = "1.2.840.114350.1.13.99998.8734"
+		def testAuthorityName = "Good Health Clinic"
+		new HealthEntity(testAuthorityOID, testAuthorityName).save(flush: true, failOnError: true)
+		
 		def country = Country.findOrCreateWhere(name: "AR").save(failOnError: true, flush: true)
 		def province = Province.findOrCreateWhere(country: country, name: "AR-S").save(failOnError: true, flush: true)
 		City.findOrCreateWhere(province: province, name: "Venado Tuerto").save(failOnError: true, flush: true)
