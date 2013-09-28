67749a1b6273e12434caf5fc892c4c9e3f1683f7
Joaquin Magneres <joacomag@gmail.com>
2013-09-28 19:55:57 -0300

    Modifico Autor

--- src/regdoc/grails-app/domain/com/janpix/regdoc/domain/Author.groovy
+++ src/regdoc/grails-app/domain/com/janpix/regdoc/domain/Author.groovy
@@ -2,7 +2,5 @@ package com.janpix.regdoc.domain
 
 class Author {
 	HealthEntity institution
-	String authorPerson
-	String authorRole
-	String authorSpecialty
+	Person person
 }
---
+++ src/regdoc/grails-app/domain/com/janpix/regdoc/domain/Person.groovy
@@ -0,0 +1,10 @@
+package com.janpix.regdoc.domain
+
+class Person {
+	String matricula
+	String nombre
+	String specialty
+	String role
+	String email
+	String phone
+}
