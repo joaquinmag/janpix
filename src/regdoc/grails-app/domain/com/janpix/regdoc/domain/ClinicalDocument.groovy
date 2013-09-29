d41481b6cc4326194c38b284cd7deb556a7e76e6
Joaquin Magneres <joacomag@gmail.com>
2013-09-29 15:14:16 -0300

    Format

--- src/regdoc/grails-app/domain/com/janpix/regdoc/domain/ClinicalDocument.groovy
+++ src/regdoc/grails-app/domain/com/janpix/regdoc/domain/ClinicalDocument.groovy
@@ -13,22 +13,22 @@ class ClinicalDocument {
 	Author author // El autor del documento
 	FileAttributes file // Atributos que pertenecen unicamente al archivo
 	DocumentState state // Estado asignado por los auditores
-	
+
 	String comments // Comentarios sobre el documento
 	String language
-	
+
 	Date documentCreationStarted
 	Date documentCreationEnded
-	
+
 	ClinicalDocumentType documentType // Tipo de documento
 	FormatType format // Indica el formato del documento. Tiene algo que ver con el formato del archivo, pero también tiene que ver con el tipo de documento.
-	
-	static hasMany = [ 
-		relatedTo: DocumentRelationship		
+
+	static hasMany = [
+		relatedTo: DocumentRelationship
 	]
 	DocumentRelationship relatedFrom
 	static mappedBy = [ relatedTo: 'parentDocument', relatedFrom: 'relatedDocument' ]
-	
+
 	static mapping = {
 		state type: DocumentStateUserType, {
 			column name: "state"
