94366cd59033ce1890b561bc32e5dadc2e3d5d4e
Joaquin Magneres <joacomag@gmail.com>
2013-10-05 18:24:28 -0300

    Hago WS de registro de documentos para regdoc

--- src/regdoc/grails-app/services/regdoc/RegistroService.groovy
+++ src/regdoc/grails-app/services/regdoc/RegistroService.groovy
@@ -1,11 +1,26 @@
 package regdoc
 
+import javax.jws.WebMethod
+import javax.jws.WebParam
+import javax.jws.soap.SOAPBinding
+import javax.jws.soap.SOAPBinding.ParameterStyle
+
+import org.apache.commons.lang.NotImplementedException
+import org.apache.cxf.annotations.WSDLDocumentation
+import org.grails.cxf.utils.EndpointType
+import org.grails.cxf.utils.GrailsCxfEndpoint
+
+import com.janpix.regdoc.domain.ClinicalDocument
 import grails.transaction.Transactional
 
 @Transactional
+@SOAPBinding(parameterStyle=ParameterStyle.BARE)
+@GrailsCxfEndpoint(expose = EndpointType.JAX_WS,soap12=true)
 class RegistroService {
 
-    def serviceMethod() {
-
-    }
+	@WebMethod
+	public void registerDocument(@WebParam(name = "clinicalDocument")ClinicalDocument clinicalDocument) {
+		
+	}
+	
 }
