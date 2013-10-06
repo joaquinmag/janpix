70ddb4528f2f2effa9808a51b29bdd9d9224b91e
Joaquin Magneres <joacomag@gmail.com>
2013-10-05 23:12:43 -0300

    Modifico assembler de clinicaldocumentdto

--- src/regdoc/src/groovy/com/janpix/regdoc/infrastructure/ClinicalDocumentAssembler.groovy
+++ src/regdoc/src/groovy/com/janpix/regdoc/infrastructure/ClinicalDocumentAssembler.groovy
@@ -6,7 +6,10 @@ import com.janpix.servidordocumentos.dto.ClinicalDocumentDTO
 class ClinicalDocumentAssembler {
 
 	static ClinicalDocumentDTO toDTO(ClinicalDocument domainDocument) {
-		
+		ClinicalDocumentDTO dto = new ClinicalDocumentDTO()
+		dto.uniqueId = domainDocument.uniqueId
+		dto.patientId = domainDocument.patientId
+		dto.name = domainDocument.title
 	}
 	
 	static ClinicalDocument fromDTO(ClinicalDocumentDTO dtoDocument) {
