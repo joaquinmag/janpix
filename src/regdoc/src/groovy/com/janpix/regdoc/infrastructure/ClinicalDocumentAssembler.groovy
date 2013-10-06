07df5bea2cba3f12b14362cf40aca1b609104c2b
Joaquin Magneres <joacomag@gmail.com>
2013-10-05 21:29:11 -0300

    Agrego imports

--- src/regdoc/src/groovy/com/janpix/regdoc/infrastructure/ClinicalDocumentAssembler.groovy
+++ src/regdoc/src/groovy/com/janpix/regdoc/infrastructure/ClinicalDocumentAssembler.groovy
@@ -1,5 +1,8 @@
 package com.janpix.regdoc.infrastructure
 
+import com.janpix.regdoc.domain.ClinicalDocument
+import com.janpix.servidordocumentos.dto.ClinicalDocumentDTO
+
 class ClinicalDocumentAssembler {
 
 	static ClinicalDocumentDTO toDTO(ClinicalDocument domainDocument) {
