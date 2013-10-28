d681a5c50ad4cd9e76b7d017c93a395452ca79c1
Joaquin Magneres <joacomag@gmail.com>
2013-10-28 00:46:06 -0300

    Aumento version plugin.

--- src/modulos-servidor-documentos/ModulosServidorDocumentosGrailsPlugin.groovy
+++ src/modulos-servidor-documentos/ModulosServidorDocumentosGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ModulosServidorDocumentosGrailsPlugin {
     // the plugin version
-    def version = "0.15"
+    def version = "0.16"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
