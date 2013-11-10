9d8e59c0003487002a80d4bad4d1a30b399ce61f
Joaquin Magneres <joacomag@gmail.com>
2013-11-10 20:13:55 -0300

    Elimino plugins innecesarios de repodoc

--- src/repodoc/grails-app/conf/BuildConfig.groovy
+++ src/repodoc/grails-app/conf/BuildConfig.groovy
@@ -57,7 +57,6 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.42"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.0"
         compile ':cache:1.1.1'
         compile ":modulos-servidor-documentos:0.16"
 		compile ":mongodb:1.3.0"
@@ -67,11 +66,5 @@ grails.project.dependency.resolution = {
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.1" // or ":hibernate4:4.1.11.1"
         runtime ":database-migration:1.3.5"
-        runtime ":jquery:1.10.2"
-        runtime ":resources:1.2"
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-        //runtime ":yui-minify-resources:0.1.5"
     }
 }
